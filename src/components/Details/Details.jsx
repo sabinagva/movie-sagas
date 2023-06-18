23a0bb549756ee0bf240ca98a805ee6d7868f176
Sabina Gahramanova <sabina.ghrmnli@gmail.com>
2023-06-18 10:44:13 -0500

    finished details page (non-functioning yet)

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,11 +1,29 @@
-import { useSelector } from "react-redux"
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux"
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
+
 
 function Details () {
+//getting genre and details reducers from index store
+    const history = useHistory();
+    const dispatch = useDispatch();
+    detailsReducer = useSelector(store => store.details);
+    genresReducer = useSelector(store => store.genres);
 
-    detailsReducer = useSelector(store => store.details)
-    genresReducer = useSelector(store => store.genres)
+    //
+    useEffect(() => {
+        dispatch({type: 'GET_DETAILS', payload: detailsReducer.id})
+    }, [])
 
+    //this button enable user to go back to movie list
+   const handleClick = () => {
+    history.pushState('/')
+   }
+
+   //bellow we will be rendering movie genres and description to dom
     return (
+        <>
+        <button onClick={handleClick}>Movie list</button>
         <div>
             {genresReducer.map((genre) => (
                 <p>
@@ -17,6 +35,7 @@ function Details () {
             <img src={detailsReducer.poster}></img>
             <h1>{detailsReducer.description}</h1>
         </div>
+        </>
     )
 
 }
